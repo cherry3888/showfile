e048fd3ddac0b3ba9a15fb5cee0d4db0d737232b
victor.cheremisin@gmail.com <Pon@856Lord>
2023-07-31 16:55:09 +0300

    Добавил Spring Security

---
+++ src/main/java/com/example/showfile/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.showfile.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(auth -> auth.antMatchers("/**").permitAll())
+                .csrf().disable()
+                .cors()
+        //        .and().headers().defaultsDisabled().contentTypeOptions(); //Not work
+                .and().headers().disable();
+        ;
+    }
+
+}
