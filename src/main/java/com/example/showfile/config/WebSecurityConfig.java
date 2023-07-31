f113f4dd4324822d27f90012c1141b05d296c157
victor.cheremisin@gmail.com <Pon@856Lord>
2023-07-31 16:55:09 +0300

    Добавил Spring Security

---
+++ src/main/java/com/example/showfile/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.showfile.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(auth -> auth.antMatchers("/**").permitAll())
+                .csrf().disable()
+                .cors();
+    }
+
+}
--- src/main/java/com/example/showfile/controllers/ShowFileController.java
+++ src/main/java/com/example/showfile/controllers/ShowFileController.java
@@ -1,12 +1,12 @@
 package com.example.showfile.controllers;
 
 import com.example.showfile.services.FileStorageService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
